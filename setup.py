5805b8fd79d4e34b3543bee2fc65ecf4fc4b34ab
Jean-François Roche <jfroche@affinitic.be>
2010-06-09 14:44:12 +0200

    inital package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='gdw.stats',
+      version=version,
+      description="GDW Stats",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Jean Francois Roche',
+      author_email='jfroche@affinitic.be',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages=['gdw'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ])
