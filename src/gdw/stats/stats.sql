1ed2afbfe06d2a5e9e75c4d8eab0982b1907e66f
Jean-François Roche <jfroche@affinitic.be>
2010-08-26 16:34:03 +0200

    add view sql

--- src/gdw/stats/stats.sql
+++ src/gdw/stats/stats.sql
@@ -1,2 +1,3 @@
 CREATE USER test WITH PASSWORD 'test' CREATEDB;
 CREATE DATABASE gites_wallons_test OWNER test;
+create view blockinghistory as select heb_blockhistory_heb_pk as heb_pk, heb_blockhistory_blocked_dte as block_start, case when heb_blockhistory_activated_dte is null then current_date else heb_blockhistory_activated_dte end as block_end from heb_blocking_history;
